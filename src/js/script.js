fd410ac286d76ec390d7573d83834c140388debf
Lefrec <blackfox244@gmail.com>
2024-11-12 16:21:28 +0100

    ouverture/fermeture menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,11 @@
-// On cible les éléments à modifier
-const toggle = document.querySelector(".menu");
-const nav = document.querySelector(".menu-btn");
+const nav = document.querySelector(".menu");
+const toggle = document.querySelector(".menu-btn");
 
-// Ajoute un écouteur d'événements à l'élément 'toggle' qui écoute les événements 'click'.
+
 toggle.addEventListener("click", (event) => {
-    // Mise à jour des attributs ARIA pour accessibilité
-    nav.ariaExpanded = true;
-    toggle.ariaHidden = false;
+    const isOpen = toggle.ariaExpanded === "true";
+    const isClosed = !isOpen;
+    console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
+    toggle.ariaExpanded = isClosed;
+    nav.ariaHidden = isOpen;
 });
